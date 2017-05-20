7a4fe454986ca6c0155479ae1415c056ce467781
Alvian <alvian.rapm@gmail.com>
2017-05-20 21:40:15 +0700

    List Jual Mobil

---
+++ app/src/main/java/com/rapm/app/listjualmobil/MobilModel.java
@@ -0,0 +1,43 @@
+package com.rapm.app.listjualmobil;
+
+import java.io.Serializable;
+
+/**
+ * Created by Alvian on 20/05/2017.
+ */
+
+public class MobilModel implements Serializable {
+    String title, image, harga, lokasi;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+}
